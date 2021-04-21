9d11c581d66c62d15291e03eab8b883336bb1139
tanaykhandelwal8 <69707838+tanaykhandelwal8@users.noreply.github.com>
2021-04-21 15:41:07 +0530

    New package

--- assignment1-package 2/a1grader.h
+++ assignment1-package 2/a1grader.h
@@ -1,10 +1,10 @@
 //// The bare minimum definitions needed for a1grader
 //// To the student: Ignore the below code!
-#define BOOK_LENGTH 1284    /* The length of a cipher book */
-#define MAX_MSG_LENGTH 1024 /* The maximum length of an encrypted message */
-#define BLOCKSIZE 16        /* The length of a block (key, output) */
-#define N_TIMESTEPS 20      /* number of timesteps */
-#define N_OUTPUT_BLOCKS 2   /* number of output blocks */
+#define BOOK_LENGTH 1284            /* The length of a cipher book */
+#define MAX_MSG_LENGTH 1024         /* The maximum length of an encrypted message */
+#define BLOCKSIZE 16                /* The length of a block (key, output) */
+#define N_TIMESTEPS 20              /* number of timesteps */
+#define N_OUTPUT_BLOCKS 2           /* number of output blocks */
 
 typedef char book_t[BOOK_LENGTH];     // A cipherbook (1284 bytes)
 typedef unsigned char byte_t;         // A byte (8 bits)
@@ -24,21 +24,21 @@ typedef byte_t msg_t[MAX_MSG_LENGTH]; // An encrypted message (l bytes)
 // #define COLOUR_CTRL_CHARS 1
 
 // Prints a byte array of length len, in groups of 16 (BLOCKSIZE) bytes
-// For each line:
+// For each line: 
 // 1. Print the bit number of the first byte of each block, in hexadecimal
 // 2. Print the hexadecimal and ASCII representation of each block.
 // Example output for one block (len = 16)
 //  0x0000: 58 2f 50 37 97 39 c0 82  1c cc d2 73 3f 96 d5 ef   X/P7‧9‧‧‧‧‧s?‧‧‧
 //  -> the byte 2f (in hexadecimal) is byte 0x0001 in the array, and prints to
 //     the ASCII value /.
-// Control characters are converted to the symbol ‧ printed in red if
+// Control characters are converted to the symbol ‧ printed in red if 
 // COLOUR_CTRL_CHARS is defined (see the definition line above).
 void hexdump(byte_t a[], int len);
 
 //////// Submit functions
 // Submits stage 0 and prints some useful debug information.
-void submit_stage0(int cipher_length, msg_t ciphertext, block_t outputs[N_OUTPUT_BLOCKS],
-                   block_t timesteps[N_TIMESTEPS], book_t cipherbook);
+void submit_stage0(int cipher_length, msg_t ciphertext,block_t outputs[N_OUTPUT_BLOCKS], 
+                    block_t timesteps[N_TIMESTEPS], book_t cipherbook);
 // Submits stage 1 and prints some useful debug information.
 void submit_stage1(book_t stripped_book, int book_len);
 // Submits stage 2 and prints some useful debug information.
@@ -48,4 +48,3 @@ void submit_stage3(byte_t *key1);
 // Submits stage 4 and prints some useful debug information.
 void submit_stage4(msg_t plaintext);
 
-void enable_stage_testing(int argc, char *argv[]);
